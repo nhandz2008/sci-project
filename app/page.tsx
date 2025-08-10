90b5a79fd90e7a20693ffb87a0dd65852f900bf8
Canh Nhan <nguyencanhnhan2008@gmail.com>
2025-08-10 09:11:38 +0700

    add carousel to the current page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/hero";
 import STEMInfo from "../components/stem-info";
+import FeaturedCarousel from "../components/featured-carousel";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
       <STEMInfo />
+      <FeaturedCarousel />
     </main>
   );
 } 
